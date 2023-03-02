980aa291e4922ea21677a8caeccc448d15155905
Nazareh Velho Turmina <nturmina@maiatechnology.com.au>
2023-03-02 17:08:49 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "@/index.css";
-import Notifications from "@kyvg/vue3-notification";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).use(Notifications).mount("#app");
+import './assets/main.css'
+
+createApp(App).mount('#app')
